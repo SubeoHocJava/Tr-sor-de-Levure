24ad96a70d463d064dcfd5cf1bfe149525f427c4
hien <22130080@st.hcmuaf.edu.vn>
2024-12-06 18:23:14 +0700

    rating product

--- profile/rating.js
+++ profile/rating.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     var normalStar = '&#9734;'; // Empty star
     var filledStar = '&#9733;'; // Filled star
-    var rating = 0;
 
     // Handle click on a star
+    $('.stars').each(function(){
+        getRating(this);
+    });
     $('.star').click(function () {
-        var index = $(this).index(); // Get the index of the clicked star
-        rating = index + 1; // Update the rating value
-        $(this).prevAll().html(filledStar).addClass('star_active');
+        var index = $(this).parent('.stars').children('.star').index(this); // Get the index of the clicked star
+        $(this).parent('.stars').addClass('stars-selected');
+        $(this).parent('.stars').data('value', index + 1);
+        $(this).prevAll().html(filledStar).addClass('stars_active');
         $(this).html(filledStar).addClass('star_active');
         $(this).nextAll().html(normalStar).removeClass('star_active');
     });
@@ -21,12 +24,16 @@ $(document).ready(function () {
 
     // Handle mouse leaving the stars area
     $('.stars').mouseleave(function () {
-        $('.star').each(function (index) {
+        getRating(this);
+    });
+    function getRating(stars) {
+        var rating = $(stars).data('value');
+        $(stars).children('.star').each(function (index) {
             if (index < rating) {
-                $(this).html(filledStar).addClass('star_active'); // Filled star
+                $(this).html(filledStar).addClass('star_active');
             } else {
-                $(this).html(normalStar).removeClass('star_active'); // Empty star
+                $(this).html(normalStar).removeClass('star_active');
             }
         });
-    });
+    }
 });
