73545a8e06b542d80f0338c059a87964fc87984d
Broccoli <22130191@st.hcmuaf.edu.vn>
2024-12-13 22:16:14 +0700

    fix merge

--- Web_WineShop/Web_WineShop/obj/Debug/net8.0/Web_WineShop.AssemblyInfo.cs
+++ Web_WineShop/Web_WineShop/obj/Debug/net8.0/Web_WineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b718b963a8192703d175460e55a06d8cfad9daa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793fdd61dc5dd91e3b5bc50f6df03143163d4eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
