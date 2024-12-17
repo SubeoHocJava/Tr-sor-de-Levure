e16748d25413bd3eb901b17b33f3f0cf117c6360
Ngoc Thien aka Subeo <22130262@st.hcmuaf.edu.vn>
2024-12-17 20:09:53 +0700

    tao view Gin

---
+++ Web_WineShop/Web_WineShop/Controllers/GinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_WineShop.Controllers
+{
+    public class GinController : Controller
+    {
+        public IActionResult Gin()
+        {
+            return View();
+        }
+    }
+}
