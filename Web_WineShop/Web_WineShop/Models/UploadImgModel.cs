483da14c77cf378a3831eeb25234ebc4a959c51c
hien <22130080@st.hcmuaf.edu.vn>
2024-12-24 17:17:27 +0700

    upload ảnh, folder chứa ảnh

---
+++ Web_WineShop/Web_WineShop/Models/UploadImgModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace Web_WineShop.Models
+{
+	public class UploadImgModel
+	{
+		static List<string> validExtensions = new List<string> { ".jpg", ".jpeg", ".png", ".gif", ".pdf" };
+		public async static Task<List<String>> Upload(List<IFormFile> files)
+		{
+			List<String> result = new List<string>(files.Count);
+			foreach (IFormFile file in files)
+			{
+				if (!validExtensions.Contains(Path.GetExtension(file.FileName)))
+				{
+					throw new NotSupportedException("Invalid file type.");
+				}
+				var filePath = Guid.NewGuid().ToString() + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + Path.GetExtension(file.FileName);
+				filePath = Path.Combine("img", filePath);
+
+				using (var stream = System.IO.File.Create(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", filePath)))
+				{
+					await file.CopyToAsync(stream);
+				}
+				result.Add(filePath);
+
+			}
+			return result;
+		}
+	}
+}
