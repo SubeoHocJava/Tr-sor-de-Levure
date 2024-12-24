5f0873e85b2d4efe90f7c964eb6cc87acd17220d
hien <22130080@st.hcmuaf.edu.vn>
2024-12-24 17:18:22 +0700

    model biding cho profile page

---
+++ Web_WineShop/Web_WineShop/Models/ProfileModel/BankAccountModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_WineShop.Models.ProfileModel
+{
+	public class BankAccountModel
+	{
+		public ICollection<BankAccount>? BankAccounts { get; set; }
+		public int? BankAccountDefaultId { get; set; }
+		public bool IsDefaultBank(BankAccount bankAccount)
+		{
+			return BankAccountDefaultId == bankAccount.Id;
+
+		}
+
+		public BankAccountModel(ICollection<BankAccount>? bankAccounts, int? bankAccountDefaultId)
+		{
+			BankAccounts = bankAccounts;
+			BankAccountDefaultId = bankAccountDefaultId;
+		}
+	}
+}
---
+++ Web_WineShop/Web_WineShop/Models/ProfileModel/UserDetailModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_WineShop.Models.ProfileModel
+{
+	public class UserDetailModel
+	{
+		public int Id { get; set; }
+		[Required]
+		public string FullName { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public DateTime DateOfBirth { get; set; }
+		[Required]
+		public string Phone { get; set; }
+		[Required]
+		public string ReceiveAddress { get; set; }
+
+		public UserDetailModel(int id, string fullName, string email, DateTime dateOfBirth, string phone, string receiveAddress)
+		{
+			Id = id;
+			FullName = fullName;
+			Email = email;
+			DateOfBirth = dateOfBirth;
+			Phone = phone;
+			ReceiveAddress = receiveAddress;
+		}
+	}
+}
